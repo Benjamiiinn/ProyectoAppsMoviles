bc068e5ef2321465e1c7168f504bf550dcaf297d
Benjamiiinn <bi.gonzalezg@duocuc.cl>
2025-12-17 02:11:01 -0300

    URL backend render

--- app/src/main/java/com/example/proyectomoviles/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/proyectomoviles/remote/RetrofitClient.kt
@@ -1,11 +1,29 @@
 package com.example.proyectomoviles.remote
 
+import com.example.proyectomoviles.utils.TokenManager
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // URL del backend real en Render
-    private const val BASE_URL = "http://backendappmovil.onrender.com/"
+    private const val BASE_URL = "https://backendappmovil.onrender.com/"
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val token = TokenManager.getToken()
+
+            val newRequest = if (token != null) {
+                // Si hay token, lo agregamos al header
+                originalRequest.newBuilder()
+                    .header("Authorization", "Bearer $token")
+                    .build()
+            } else {
+                originalRequest
+            }
+            chain.proceed(newRequest)
+        }
+        .build()
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
